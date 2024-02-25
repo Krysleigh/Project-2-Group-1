9bf6b6cff06750fd6f3cd79ef3f6df36dbe8bde6
Chaitanya Kota <chaitanya.kota@outlook.com>
2024-02-24 21:28:26 -0800

    Committing schema for crowdfunding db

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,59 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Category" (
+    "category_id" VARCHAR(255)   NOT NULL,
+    "category_name" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "Subcategory" (
+    "subcategory_id" VARCHAR(255)   NOT NULL,
+    "subcategory_name" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "Campaign" (
+    "cf_id" INTEGER   NOT NULL,
+    "contact_id" INTEGER   NOT NULL,
+    "company_name" TEXT   NOT NULL,
+    "description" TEXT   NOT NULL,
+    "goal" REAL   NOT NULL,
+    "pledged" REAL   NOT NULL,
+    "outcome" VARCHAR(50)   NOT NULL,
+    "backers_count" INTEGER   NOT NULL,
+    "country" VARCHAR(2)   NOT NULL,
+    "currency" VARCHAR(3)   NOT NULL,
+    "launch_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR(255)   NOT NULL,
+    "subcategory_id" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_Campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "Contacts" (
+    "contact_id" INTEGER   NOT NULL,
+    "first_name" VARCHAR(255)   NOT NULL,
+    "last_name" VARCHAR(255)   NOT NULL,
+    "email" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "Contacts" ("contact_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "Category" ("category_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Subcategory" ("subcategory_id");
+
